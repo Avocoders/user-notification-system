34823a4f68f27929d5dd7e6295a45b29ec25e4c8
Veronika Belova <belovaveronika14@gmail.com>
2026-09-09 19:52:27 +0300

    add test UserEvent created and add plugin Lombok in user-service-spring

---
+++ user-service-spring/src/test/java/io/github/avocoders/userservicespring/event/UserEventTest.java
@@ -0,0 +1,20 @@
+package io.github.avocoders.userservicespring.event;
+
+import org.junit.jupiter.api.Test;
+
+import static io.github.avocoders.userservicespring.event.UserOperation.CREATED;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserEventTest {
+
+    @Test
+    void createdEvent() {
+        Long id = 15L;
+        String name = "Veronika";
+        String email = "veronika@ya.ru";
+
+        UserEvent event = new UserEvent(CREATED,id, name, email);
+        assertEquals("veronika@ya.ru", event.email());
+        assertEquals(CREATED, event.operation());
+    }
+}
