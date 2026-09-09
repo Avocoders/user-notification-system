064b8d21bb5cfd93718a345cdee26ff2918e5be0
Veronika Belova <belovaveronika14@gmail.com>
2026-09-09 14:03:22 +0300

    Initial commit: user-service-spring

---
+++ user-service-spring/src/main/java/io/github/avocoders/userservicespring/entity/User.java
@@ -0,0 +1,40 @@
+package io.github.avocoders.userservicespring.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private Integer age;
+    @Column(name="created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    public User(String name, String email, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    @PrePersist
+    private void initializeCreatedAt() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
