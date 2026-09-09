71d149bd312238c319c23532ad9207dc4b526b92
Veronika Belova <belovaveronika14@gmail.com>
2026-09-09 19:12:27 +0300

    add dependency, enum and record in user-service-spring

---
+++ user-service-spring/src/main/java/io/github/avocoders/userservicespring/event/UserEvent.java
@@ -0,0 +1,9 @@
+package io.github.avocoders.userservicespring.event;
+
+public record UserEvent(
+        UserOperation operation,
+        Long userId,
+        String name,
+        String email
+) {
+}
---
+++ user-service-spring/src/main/java/io/github/avocoders/userservicespring/event/UserOperation.java
@@ -0,0 +1,6 @@
+package io.github.avocoders.userservicespring.event;
+
+public enum UserOperation {
+    CREATED,
+    DELETED
+}
