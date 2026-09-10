4e3abe8c7324c075954fe72da3f85d50b8a6df01
Veronika Belova <belovaveronika14@gmail.com>
2026-09-11 00:21:26 +0300

    rewrite UserEventListener in notification-service

--- notification-service/src/main/java/io/github/avocoders/notificationservice/kafka/UserEventListener.java
+++ notification-service/src/main/java/io/github/avocoders/notificationservice/kafka/UserEventListener.java
@@ -1,13 +1,39 @@
 package io.github.avocoders.notificationservice.kafka;
 
 import io.github.avocoders.notificationservice.event.UserEvent;
+import io.github.avocoders.notificationservice.service.EmailNotificationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserEventListener {
+    private final EmailNotificationService emailNotificationService;
+
     @KafkaListener(topics = "user-events")
     public void listen(UserEvent userEvent) {
+        String subject;
+        String body;
+
+        switch (userEvent.operation()) {
+            case CREATED -> {
+                subject = "Ваша учетная запись создана";
+                body = "Создан пользователь " + userEvent.userId();
+            }
+            case DELETED -> {
+                subject = "Ваша учетная запись удалена";
+                body = "Удален пользователь " + userEvent.userId();
+            }
+            default -> throw new IllegalStateException(
+                    "Неизвестная операция: " + userEvent.operation()
+            );
+        }
+         emailNotificationService.sendEmail(
+                 userEvent.email(),
+                 subject,
+                 body
+         );
         System.out.println(userEvent);
     }
 }
