a848f73ff1142f4c556e0878714187082d864a17
Veronika Belova <belovaveronika14@gmail.com>
2026-09-10 23:33:11 +0300

    rewrite  EmailNotificationService in notification-service

--- notification-service/src/main/java/io/github/avocoders/notificationservice/service/EmailNotificationService.java
+++ notification-service/src/main/java/io/github/avocoders/notificationservice/service/EmailNotificationService.java
@@ -1,11 +1,22 @@
 package io.github.avocoders.notificationservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @RequiredArgsConstructor
 public class EmailNotificationService {
     private final JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        javaMailSender.send(message);
+    }
 }
