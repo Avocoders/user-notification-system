dfe8dc86df8aec4e8bea9072ea1c87efaf238da9
Veronika Belova <belovaveronika14@gmail.com>
2026-09-15 19:55:10 +0300

    add FallbackController in api-gateway

--- api-gateway/src/main/java/io/github/avocoders/apigateway/controller/FallbackController.java
+++ api-gateway/src/main/java/io/github/avocoders/apigateway/controller/FallbackController.java
@@ -1,7 +1,18 @@
 package io.github.avocoders.apigateway.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 
 @RestController
 public class FallbackController {
+    @GetMapping("/fallback/users")
+    public ResponseEntity<Map<String, String>> userFallback() {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(Map.of("message", "Сервис недоступен"));
+    }
 }
